a782350cb1005fd659d3ddc62abb7a1dc77cd6de
Lyrakun <273812235+Lyrakunai@users.noreply.github.com>
2026-05-11 22:36:47 +0530

    Add files via upload

---
+++ day31_list_practice.py
@@ -0,0 +1,32 @@
+# Day 31 - Lists
+
+# Creating a list
+fruits = ["apple", "banana", "mango"]
+
+
+# Accessing items
+bag = ["pen","pencil","bottle"]
+print("First item:", bag[0])
+print("third item:",bag[2])
+
+
+# Negative indexing
+marks = [45,80,63,90]
+print("last item:",marks[-1])
+
+
+# Updating item
+numbers = [2,3,6,8]
+numbers[1] = 4
+print("Updated list:", numbers)
+
+
+# Adding item using append()
+fruits = ["apple","banana","mango"]
+fruits.append("orange")
+print("After append:", fruits)
+
+
+# Total items using len()
+fruits = ["apple","banana","mango"]
+print("Total items:", len(fruits))
